7cd5dc4b7173c6572196a5e474371fb555c1dd48
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-02-23 15:58:01 -0300

    adding TextInput

--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   ImageBackground,
   TextInput,
-  Image
+  Image,
 } from "react-native";
 
 // estrutura do app
@@ -14,29 +14,38 @@ export default function App() {
   return (
     <ImageBackground
       source={require("../assets/images/fundoGradiente.jpg")}
+      resizeMode="cover"
       style={styles.fundo}
+      blurRadius={15}
     >
       <View style={styles.container}>
-        <Text style={styles.texto}>NextIn</Text>
+        <Image
+          source={require("../assets/images/logoNext.png")}
+          style={styles.logo}
+        />
         <View style={styles.box}>
-          <ImageBackground source={require("../assets/images/login-bg.jpg")}
-          style={styles.fundoBox}
-          >
-            <Image source={require('../assets/images/perfil.png')}
+          {/*<ImageBackground
+            source={require("../assets/images/login-bg.jpg")}
+            style={styles.fundoBox}
+          >*/}
+          <Image
+            source={require("../assets/images/perfil.png")}
             style={styles.imgPerfil}
-            />
-            <Text style={styles.login}>Login</Text>
-            <TextInput
-              placeholder="Usuário"
-              placeholderTextColor={"#999"}
-              style={styles.input}
-            ></TextInput>
-            <TextInput
-              placeholder="Senha"
-              placeholderTextColor={"#999"}
-              style={styles.input}
-            ></TextInput>
-          </ImageBackground>
+          />
+          <Text style={styles.login}>Login</Text>
+          <View style={styles.containerInput}>
+            <Text style={styles.labelName}>Usuário</Text>
+            <TextInput style={styles.input}></TextInput>
+          </View>
+          <View style={styles.containerInput}>
+            <Text style={styles.labelName}>Senha</Text>
+            <TextInput style={styles.input}></TextInput>
+          </View>
+          <View style={styles.containerCheck}>
+            <View style={styles.quadrado}></View>
+            <Text style={styles.labelCheck}> lembrar-me </Text>
+          </View>
+          {/*</ImageBackground>*/}
         </View>
       </View>
     </ImageBackground>
@@ -46,9 +55,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
   },
+
   texto: {
     fontSize: 20,
     fontWeight: "bold",
@@ -62,10 +71,12 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    backgroundColor: "#FFF",
+    backgroundColor: "#000",
     padding: 5,
     borderRadius: 5,
-    width: "60%",
+    borderWidth: 0.5,
+    borderColor: "#FFF",
+    width: "100%",
     height: 35,
     marginBottom: 10,
   },
@@ -75,29 +86,72 @@ const styles = StyleSheet.create({
     width: "80%",
     height: "40%",
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#999",
+    backgroundColor: "#000",
   },
 
-  fundoBox:{
+  fundoBox: {
     flex: 1,
     width: "100%",
-    height: '100%',
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
-
   },
 
   login: {
     fontSize: 28,
     marginBottom: 10,
     fontWeight: "bold",
-    color: '#FFF',
+    color: "#FFF",
   },
 
-  imgPerfil:{
-    width:80,
+  imgPerfil: {
+    width: 80,
     height: 80,
-    top: 0,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+
+  logo: {
+    marginTop: 90,
+    marginBottom: 50,
+    width: 300,
+    height: 100,
+  },
+
+  labelName: {
+    position: "absolute",
+    color: "#FFF",
+    zIndex: 10,
+    top: -8,
+    left: 5,
+    backgroundColor: "#000",
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
+  },
+
+  containerInput: {
+    width: "60%",
+    alignItems: "center",
+  },
+
+  containerCheck: {
+    position: "absolute",
+    left: 68,
+    bottom: 30,
+    flexDirection: "row",
+  },
+
+  quadrado: {
+    borderWidth: 0.5,
+    borderColor: "#FFF",
+    borderRadius: 3,
+    width: 15,
+    height: 15,
+    marginRight: 5,
+  },
+
+  labelCheck: {
+    color: "#FFF",
   },
 });
