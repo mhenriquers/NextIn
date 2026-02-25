686273150855742b3db0ef0cc2fc9aa11f91fda5
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-02-25 15:55:53 -0300

    adding background

---
+++ app/screens/home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native'; 
+
+
+
+export default function Home (){
+    <View style={styles.container} >
+        <Text>
+            Olá Home
+        </Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+});
--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -8,99 +8,115 @@ import {
   Image,
   TouchableOpacity,
   Pressable,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
+  import { Video, ResizeMode, } from 'expo-av';
 
 // estrutura do app
 
 export default function App() {
   const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [lembrar, setLembrar] = useState(false);
 
   return (
-    <ImageBackground
-      source={require("../assets/images/fundoGradiente.png")}
-      resizeMode="cover"
+    <View style={{flex: 1}}>
+    <Video
+      source={require("../assets/videos/videoFundo1.mp4")}
       style={styles.fundo}
-      blurRadius={20}
-    >
-      <View style={styles.container}>
-        <Image
-          source={require("../assets/images/logoNext.png")}
-          style={styles.logo}
-        />
-        <View style={styles.box}>
-          {/*<ImageBackground
+      resizeMode={ResizeMode.COVER}
+      shouldPlay
+      isLooping
+      isMuted
+      />
+
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Image
+            source={require("../assets/images/logoNext.png")}
+            style={styles.logo}
+          />
+          <View style={styles.box}>
+            {/*<ImageBackground
             source={require("../assets/images/login-bg.jpg")}
             style={styles.fundoBox}
           >*/}
-          <Image
-            source={require("../assets/images/perfil.png")}
-            style={styles.imgPerfil}
-          />
-          <Text style={styles.login}>Login</Text>
-          <View style={styles.containerInput}>
-            <Text style={styles.labelName}>Usuário</Text>
+            <Image
+              source={require("../assets/images/perfil.png")}
+              style={styles.imgPerfil}
+            />
+            <Text style={styles.login}>Login</Text>
+            <View style={styles.containerInput}>
+              <Text style={styles.labelName}>Usuário</Text>
 
-            <View style={styles.container2}>
-              <Image
-                source={require("../assets/images/iconPerson.png")}
-                style={styles.iconInput}
-              />
-
-              <TextInput
-                style={styles.input}
-                placeholder="Exemplo@gmail.com"
-                placeholderTextColor={"#999"}
-              ></TextInput>
-            </View>
-          </View>
-          <View style={styles.containerInput}>
-            <Text style={styles.labelName}>Senha</Text>
-            <View style={styles.container2}>
-              <Image
-                source={require("../assets/images/iconLock.png")}
-                style={styles.iconInput}
-              />
-              <TouchableOpacity
-                style={styles.containerEye}
-                onPress={() => setMostrarSenha(!mostrarSenha)}
-              >
+              <View style={styles.container2}>
                 <Image
-                  source={
-                    mostrarSenha
-                      ? require("../assets/images/eyeSee.png")
-                      : require("../assets/images/eyeNotSee.png")
-                  }
-                  style={styles.iconEye}
+                  source={require("../assets/images/iconPerson.png")}
+                  style={styles.iconInput}
                 />
-              </TouchableOpacity>
-              <TextInput
-                secureTextEntry={!mostrarSenha}
-                style={styles.input}
-                placeholder="sua senha"
-                placeholderTextColor={"#999"}
-              ></TextInput>
-            </View>
-          </View>
-          <View style={styles.containerCheck}>
-            <View style={styles.quadrado}></View>
-            <Text style={styles.labelCheck}> lembrar-me </Text>
-            <Text style={styles.labelCheck}> Esqueci minha senha </Text>
-          </View>
-          {/*</ImageBackground>*/}
 
-          <View style={styles.containerBotao}>
-            <Pressable
-              style={({ pressed }) => [
-                styles.botao,
-                { backgroundColor: pressed ? "#2846a0" : "#3e6eff" },
-              ]}
-            >
-              <Text style={styles.botaoText}> Entrar </Text>
-            </Pressable>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Exemplo@gmail.com"
+                  placeholderTextColor={"#999"}
+                ></TextInput>
+              </View>
+            </View>
+            <View style={styles.containerInput}>
+              <Text style={styles.labelName}>Senha</Text>
+              <View style={styles.container2}>
+                <Image
+                  source={require("../assets/images/iconLock.png")}
+                  style={styles.iconInput}
+                />
+                <TouchableOpacity
+                  style={styles.containerEye}
+                  onPress={() => setMostrarSenha(!mostrarSenha)}
+                >
+                  <Image
+                    source={
+                      mostrarSenha
+                        ? require("../assets/images/eyeSee.png")
+                        : require("../assets/images/eyeNotSee.png")
+                    }
+                    style={styles.iconEye}
+                  />
+                </TouchableOpacity>
+                <TextInput
+                  secureTextEntry={!mostrarSenha}
+                  style={styles.input}
+                  placeholder="sua senha"
+                  placeholderTextColor={"#999"}
+                ></TextInput>
+              </View>
+            </View>
+            <View style={styles.containerCheck}>
+              <TouchableOpacity onPress={() => setLembrar(!lembrar)}>
+                <View style={styles.quadrado}>
+                 {lembrar &&
+                 <Image source={require("../assets/images/V.png") } style={styles.verificado} />}
+                </View>
+              </TouchableOpacity>
+              <Text style={styles.labelCheck}> lembrar-me </Text>
+              <Text style={styles.labelCheck}> Esqueci minha senha </Text>
+            </View>
+            {/*</ImageBackground>*/}
+
+            <View style={styles.containerBotao}>
+              <Pressable
+                style={({ pressed }) => [
+                  styles.botao,
+                  { backgroundColor: pressed ? "#2846a0" : "#3e6eff" },
+                ]}
+              >
+                <Text style={styles.botaoText}> Entrar </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
+      </TouchableWithoutFeedback>
       </View>
-    </ImageBackground>
+
   );
 }
 
@@ -119,7 +135,12 @@ const styles = StyleSheet.create({
   },
 
   fundo: {
-    flex: 1,
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: -1,
   },
 
   input: {
@@ -140,7 +161,7 @@ const styles = StyleSheet.create({
     width: "80%",
     height: "50%",
     alignItems: "center",
-    backgroundColor: "#000",
+    backgroundColor: "rgba(0,0,0,0.55)",
   },
 
   fundoBox: {
@@ -194,6 +215,8 @@ const styles = StyleSheet.create({
     left: 50,
     bottom: 100,
     flexDirection: "row",
+    alignContent: 'center',
+    width: "70%",
   },
 
   quadrado: {
@@ -202,12 +225,23 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     width: 15,
     height: 15,
-    marginRight: 5,
+    margin: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 10,
+  },
+
+  verificado:{
+    height: 15,
+    width: 15,
+    zIndex: 5,
   },
 
   labelCheck: {
     color: "#fff",
     marginRight: 24,
+    alignSelf: 'center',
+
   },
 
   iconInput: {
