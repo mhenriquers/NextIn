a8c00ca7060290e8a966ec188ef35127ea8b72a8
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-03-07 10:45:13 -0300

    simplifying stuff

--- App.js
+++ App.js
@@ -17,23 +17,4 @@ export default function App() {
       </NavigationContainer>
     </AuthProvider>
   );
-}import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider, useAuth } from "./src/context/AuthContext";
-import TabNavigator from "./src/navigation/TabNavigator";
-import AuthNavigator from "./src/navigation/AuthNavigator";
-
-function RootNavigator() {
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? <TabNavigator /> : <AuthNavigator />;
-}
-
-export default function App() {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </AuthProvider>
-  );
 }
