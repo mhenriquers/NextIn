79b1d015b93d31cf3350f564894234be7c4a5459
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-03-07 10:55:03 -0300

    adjusting navigators

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider, useAuth } from "./src/context/AuthContext";
-import TabNavigator from "./src/navigation/TabNavigator";
-import AuthNavigator from "./src/navigation/AuthNavigator";
-
-function RootNavigator() {
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? <TabNavigator /> : <AuthNavigator />;
-}
+import { AuthProvider } from "./src/context/AuthContext";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
   return (
