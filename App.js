1fc1d08e925f6d1c6ec9a7bc6a3d3c303a695cd6
Matheus Henrique <matheushenriqueowdail@gmail.com>
2026-03-16 13:12:19 -0300

    refactor: new dependencies

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider } from "./src/context/AuthContext";
-import RootNavigator from "./src/navigation/RootNavigator";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
